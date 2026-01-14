a590f5f5da3740581aa2e63faf97ed6b9bdd77c9
Dude6561 <nisbis194@gmail.com>
2026-01-14 16:34:44 +0545

    successfully made rag model and increased context data for better answer

--- main.py
+++ main.py
@@ -1,39 +1,41 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from google import genai
+from pydantic import BaseModel
+
 from data import documents
-from fastapi import FastAPI 
-# from pydantic import BaseModel 
-# import os
-# from google import genai
-# from dotenv import load_dotenv
-from typing import List
 from vector_store import add_document, query_document
 
-
-# load_dotenv()
-# key = os.getenv("GEMINI_API_KEY")
-# client = genai.Client(api_key=key)
-# app = FastAPI()
-# class Query(BaseModel):
-#     question:str
-
-# @app.post("/ask")
-# async def ask_question(query: Query):
-#     response = client.models.generate_content(
-#     model="gemini-2.5-flash", contents=query.question
-#    )
-#     print(response.text)
-
 app = FastAPI()
+
+load_dotenv()
+key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=key)
+
+
+class Query(BaseModel):
+    question: str
+
+
+@app.post("/ask")
+async def ask_question(query: Query):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=query.question
+    )
+    print(response.text)
+
 
 @app.on_event("startup")
 def load_data():
     add_document(documents)
 
+
 @app.get("/search")
-def search_docs(q:str):
+def search_docs(q: str):
     result = query_document(q)
-    return {
-        "Query" : q,
-        "Context": result
-    }
-
-
+    context_text = "\n".join(result)
+    prompt = f"""Answer the question using only this context:{context_text} Question: {q}If the context doesn't have the answer, say "I don't know".Answer:"""
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    return {"question": q, "answer": response.text, "context": result}
